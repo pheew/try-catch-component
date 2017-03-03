05ca3597e623846bf629b504ba77dc211ee19af2
Niek Bruins <niek@fambruins.nl>
2017-03-03 13:25:51 +0100

    Add test reporting

---
+++ run/tests.js
@@ -0,0 +1,13 @@
+var jest = require('jest-cli');
+
+
+var config = {
+    automock: false
+};
+
+if (process.env.CIRCLE_TEST_REPORTS) {
+    config.testResultsProcessor = "../node_modules/jest-junit-reporter";
+    process.env.TEST_REPORT_PATH = process.env.CIRCLE_TEST_REPORTS;
+}
+
+jest.runCLI({config: config}, './src');
